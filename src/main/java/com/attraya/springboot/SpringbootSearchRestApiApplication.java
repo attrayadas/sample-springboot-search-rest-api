df5ae9c26864ccf19dae2f4e16b0023a41e5c793
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-23 20:12:38 +0530

    Implement Swagger Documentation

--- src/main/java/com/attraya/springboot/SpringbootSearchRestApiApplication.java
+++ src/main/java/com/attraya/springboot/SpringbootSearchRestApiApplication.java
@@ -1,9 +1,33 @@
 package com.attraya.springboot;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Search REST API",
+				description = "Spring Boot Search REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Attraya",
+						email = "attrayaghoshdas@gmail.com",
+						url = "https://www.linkedin.com/in/attrayadas/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/attrayadas"
+				)
+		), externalDocs = @ExternalDocumentation(
+		description = "Spring Boot Search REST API Documentation",
+		url = "https://github.com/attrayadas/real-time-blog-application-springrest"
+)
+) // go to: http://localhost:8080/swagger-ui/index.html
 public class SpringbootSearchRestApiApplication {
 
 	public static void main(String[] args) {
