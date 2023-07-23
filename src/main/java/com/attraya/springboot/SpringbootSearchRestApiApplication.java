7c63f4231694c77b68056d1fa597fbafa11445c0
Attraya Das <attrayaghoshdas@gmail.com>
2023-07-23 20:13:55 +0530

    Change url for Swagger

--- src/main/java/com/attraya/springboot/SpringbootSearchRestApiApplication.java
+++ src/main/java/com/attraya/springboot/SpringbootSearchRestApiApplication.java
@@ -25,7 +25,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 				)
 		), externalDocs = @ExternalDocumentation(
 		description = "Spring Boot Search REST API Documentation",
-		url = "https://github.com/attrayadas/real-time-blog-application-springrest"
+		url = "https://github.com/attrayadas/springboot-search-rest-api"
 )
 ) // go to: http://localhost:8080/swagger-ui/index.html
 public class SpringbootSearchRestApiApplication {
